fc6d57c043ef2c254a0e066533c166e3b71b1eb6
Musa Rochi <musa.rochi@eps.ch>
2022-12-08 16:23:35 +0100

    fix typoo

--- wuffwau/wuff_create.py
+++ wuffwau/wuff_create.py
@@ -18,12 +18,12 @@ def wuff_create(output_dir):
     max_output_len = len(path_information)
     header_title = "YOU HAVE SUCCESSFULLY CREATED A NEW DOG!"
 
-    print_header_create_new_dog = print_header(max_output_len, header_title)
-    print_footer_create_new_dog = print_footer(max_output_len)
+    header_create_new_dog = print_header(max_output_len, header_title)
+    footer_create_new_dog = print_footer(max_output_len)
 
-    print(f"{print_header_create_new_dog}\n"
+    print(f"{header_create_new_dog}\n"
           f"|| - Name: {random_dog_name} ||\n"
           f"|| - Birth year: {random_birth_year} ||\n"
           f"|| - Sex: {random_sex} ||\n"
           f"|| - {path_information} ||\n"
-          f"{print_footer_create_new_dog}")
+          f"{footer_create_new_dog}")
